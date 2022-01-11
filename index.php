f62c34666bb75ce7b092cfa7d02eca7a801eb9a7
Illensky <alexis.laroche.02240@gmail.com>
2022-01-11 10:58:52 +0100

    exercice done

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits);"<br>";
+echo $fruits[0]."<br><br>";
 
 //Deuxieme ligne
 
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+echo "<br><br>Ben is ".$age["ben"]." years old.<br><br>";
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
-
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=".$x.", Value=".$y;
 }
 
 
@@ -21,15 +21,12 @@ echo "<br><br>Ben is " .  . " years old.<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
 
+sort($colors);
+rsort($colors);
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
 
-
-
-
-
+sort($age);
